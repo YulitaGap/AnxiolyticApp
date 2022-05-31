5f038f77aa4a4708887024595071430e4a9e5eda
Yulia Gapunovych <ugapunovich@gmail.com>
2022-05-31 17:10:32 +0300

    Set up project
     - crete empty app project
     - add app logo and accent colours

---
+++ Anxiolytic/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Anxiolytic
+//
+//  Created by Yulia Gapunovych on 08.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
